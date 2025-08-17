903e6eae00948980ffd43a437337b6b92de3c9ea
Abdelrahman Ahmed <abdelrahmanahmedonline4@gmail.com>
2025-08-17 20:45:53 +0300

    Update rabbitmq.sh

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -1,30 +1,31 @@
 #!/bin/bash
 
-sudo -i 
+
 # Install wget and RabbitMQ
 echo "Installing wget and RabbitMQ..."
-yum install wget -y
+sudo yum install wget -y
 cd /tmp/
-dnf -y install centos-release-rabbitmq-38
-dnf --enablerepo=centos-rabbitmq-38 -y install rabbitmq-server
+sudo dnf -y install centos-release-rabbitmq-38
+sudo dnf --enablerepo=centos-rabbitmq-38 -y install rabbitmq-server
 
 # Start and enable RabbitMQ service
 echo "Starting and enabling RabbitMQ service..."
-systemctl enable --now rabbitmq-server
+sudo systemctl enable --now rabbitmq-server
 
 # Setup access to user 'test' and make it admin
 echo "Configuring RabbitMQ user 'test'..."
 sudo sh -c 'echo "[{rabbit, [{loopback_users, []}]}]." > /etc/rabbitmq/rabbitmq.config'
-rabbitmqctl add_user test test
-rabbitmqctl set_user_tags test administrator
+sudo rabbitmqctl add_user test test
+sudo rabbitmqctl set_user_tags test administrator
 
 # Restart RabbitMQ service to apply changes
 echo "Restarting RabbitMQ service..."
-systemctl restart rabbitmq-server
+sudo systemctl restart rabbitmq-server
+
 # Start RabbitMQ service
 echo "Starting RabbitMQ service..."
-systemctl start rabbitmq-server
-systemctl enable rabbitmq-server
-systemctl status rabbitmq-server
+sudo systemctl start rabbitmq-server
+sudo systemctl enable rabbitmq-server
+sudo systemctl status rabbitmq-server
 
 echo "RabbitMQ setup is complete."
